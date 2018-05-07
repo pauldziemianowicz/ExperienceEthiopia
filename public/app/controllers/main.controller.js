3ef8420937b09d025e787f8f1314e09f8f708f19
pauldziemianowicz <pauldziemianowicz@gmail.com>
2018-05-07 10:07:06 -0600

    finished top of homepage elements

--- public/app/controllers/main.controller.js
+++ public/app/controllers/main.controller.js
@@ -4,4 +4,19 @@ app.controller('MainController', ['$scope', '$rootScope', 'localStorageService',
   $scope.data = {};
   $scope.uploader = {};
 
+  $scope.view.aboutUsDropdownVisible = false;
+  $scope.view.hikingToursDropdownVisible = false;
+
+  $scope.view.toggleAboutUsDropdown = function(state) {
+    $scope.view.aboutUsDropdownVisible = state;
+  };
+
+  $scope.view.toggleHikingToursDropdown = function(state) {
+    $scope.view.hikingToursDropdownVisible = state;
+  };
+
+  $scope.view.homeLearnMoreNav = function() {
+
+  };
+
 }])
