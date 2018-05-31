f7cd66da0b5ff5cb85911044de84f34cf2ad527e
pauldziemianowicz <pauldziemianowicz@gmail.com>
2018-05-31 00:32:19 -0600

    added express-sslify

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var enforce = require('express-sslify');
 
 var app = express();
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.set('case sensitive routing', true);
 // uncomment after placing your favicon in /public
